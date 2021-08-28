b5981b9297fc9400f793ddfee06f0059eb561be9
Ranieri Guimarães França <ranierig@gmail.com>
2021-08-28 04:59:49 -0300

    project

--- src/main/java/com/inarstudio/personapi/dto/request/PersonDTO.java
+++ src/main/java/com/inarstudio/personapi/dto/request/PersonDTO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -28,6 +29,7 @@ public class PersonDTO {
     private String lastName;
 
     @NotEmpty
+    @CPF
     private String cpf;
 
     private String birthDate;
--- src/main/java/com/inarstudio/personapi/mapper/PersonMapper.java
+++ src/main/java/com/inarstudio/personapi/mapper/PersonMapper.java
@@ -7,14 +7,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring")
-public abstract class PersonMapper {
+@Mapper
+public interface PersonMapper {
 
-    public static PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
     @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
-    public abstract Person toModel(PersonDTO personDTO);
+    Person toModel(PersonDTO personDTO);
 
-    public abstract PersonDTO toDTO(Person person) ;
+    PersonDTO toDTO(Person person) ;
 
 }
